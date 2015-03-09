b7eda33934032066e5a1a69a3a01fbb9596f3994
zopyx <yet@gmx.de>
2015-03-09 16:07:07 +0100

    minor cleanup

--- xmldirector/demo/demo/modsdocument_view.py
+++ xmldirector/demo/demo/modsdocument_view.py
@@ -14,8 +14,4 @@ class MODSDocument(BrowserView):
 
     def asHTML(self):
         """ Generate a demo PDF """
-        xml = self.context.xml_get('xml_content')
-        root = lxml.etree.fromstring(xml)
-        for book in root.xpath('//BIBLEBOOK')[1:]:
-            book.getparent().remove(book)
-        return lxml.etree.tostring(root)
+        return self.context.xml_get('xml_content')
