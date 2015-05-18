6485ff40a89b12b53f4c209339ab46fc5c5e18af
Andreas Jung <yet@gmx.de>
2015-05-18 20:37:39 +0200

    fix

--- democontent/setup-plone.py
+++ democontent/setup-plone.py
@@ -37,7 +37,7 @@ if 'xml-director' in app.objectIds():
     app.manage_delObjects('xml-director')
 
 try:
-    import plonetheme.sunburt
+    import plonetheme.sunburst
     addPloneSite(app, 'xml-director', create_userfolder=True, extension_ids=['plonetheme.sunburst:default', 'xmldirector.demo:default', 'pp.client.plone:default'])
 except ImportError:
     addPloneSite(app, 'xml-director', extension_ids=['plonetheme.barceloneta:default', 'xmldirector.plonecore:default', 'xmldirector.demo:default', 'pp.client.plone:default'])
